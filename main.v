7b9b62cf65e0372e8168bb92794afd37e91b68d2
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-01 16:38:45 +0800

    3rd commit

--- main.v
+++ main.v
@@ -10,3 +10,13 @@ always @(posedge clkin) begin
     clkout <= (count == 0) ? ~clkout : clkout;
 end
 endmodule
+
+module my_debouncer(input clk, input btn, output out);
+reg ff1 = 1'b0, ff2 = 1'b0;
+always @(posedge clk) begin
+    ff1 <= btn;
+    ff2 <= ff1;
+end
+assign out = ff1 & (~ff2);
+endmodule
+
