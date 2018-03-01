00a77622ea9d3cc93aa824ff206f31e140b428ab
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-01 16:38:21 +0800

    2nd commit

--- main.v
+++ main.v
@@ -1,1 +1,12 @@
 `timescale 1ns / 1ps
+
+module var_clock(
+    input[31:0] period,
+    input clkin, 
+    output reg clkout = 0);
+reg[31:0] count =  0;
+always @(posedge clkin) begin
+    count <= (count == 0) ? period - 1 : count - 1;
+    clkout <= (count == 0) ? ~clkout : clkout;
+end
+endmodule
