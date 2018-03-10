896cefd6ee51142d768d74475708754033d426df
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-10 14:20:48 +0800

    8th commit delete var_vlock

--- main.v
+++ main.v
@@ -1,16 +1,5 @@
 `timescale 1ns / 1ps
 
-module var_clock(
-    input[31:0] period,
-    input clkin, 
-    output reg clkout = 0);
-reg[31:0] count =  0;
-always @(posedge clkin) begin
-    count <= (count == 0) ? period - 1 : count - 1;
-    clkout <= (count == 0) ? ~clkout : clkout;
-end
-endmodule
-
 module my_debouncer(input clk, input btn, output out);
 reg ff1 = 1'b0, ff2 = 1'b0;
 always @(posedge clk) begin
