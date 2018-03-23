69eebf2423e8230ae35b13c2e991868396c73b92
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-23 15:21:19 +0800

    cm 4

--- main.v
+++ main.v
@@ -11,14 +11,5 @@ always @(posedge clkin) begin
 end
 endmodule
 
-module my_debouncer(input clk, input btn, output out);
-reg ff1 = 1'b0, ff2 = 1'b0;
-always @(posedge clk) begin
-    ff1 <= btn;
-    ff2 <= ff1;
-end
-assign out = ff1 & (~ff2);
-endmodule
-
 
 
