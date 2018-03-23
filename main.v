e240c36baf642825c9e3e99041202b268037fe90
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-23 15:21:11 +0800

    cm 5

--- main.v
+++ main.v
@@ -19,19 +19,6 @@ always @(posedge clk) begin
 end
 assign out = ff1 & (~ff2);
 endmodule
-module cliff_game(input clk, input[15:0] sw, input btnC, input btnU, input btnL, input btnR, input btnD, 
-                  output[15:0] led, output[6:0] seg, output[3:0] an, output dp);
-parameter STARTING_POS = 16'b0000_0001_1100_0000;
-parameter STARTING_IDX = 8'd7;
-
-reg[15:0] people = STARTING_POS;
-reg[1:0] speed = 2'b0;
-reg[1:0] dir = 2'b0;
-reg[7:0] pos = STARTING_IDX;
-reg started = 1'b0;
-reg reset = 1'b0;
-reg lose = 1'b0;
-reg[15:0] boundaries = 16'b0;
 
 
 
