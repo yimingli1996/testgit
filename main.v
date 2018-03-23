9abb4a39d01a540c6abe4694365c8f72ad0715eb
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-23 15:29:23 +0800

    cm 3

--- main.v
+++ main.v
@@ -5,11 +5,7 @@ module var_clock(
     input clkin, 
     output reg clkout = 0);
 reg[31:0] count =  0;
-always @(posedge clkin) begin
-    count <= (count == 0) ? period - 1 : count - 1;
-    clkout <= (count == 0) ? ~clkout : clkout;
-end
-endmodule
+
 
 
 
