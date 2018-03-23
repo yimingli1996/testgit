a48ab6fe64fad768de9e28c544a3235b83f9e286
LAPTOP-7TKVD1J7\YIMING <yiming.li1996@gmail.com>
2018-03-23 12:29:51 +0800

    cm 9

--- FAST.v
+++ FAST.v
@@ -25,5 +25,5 @@ module FAST(input CLOCK, output reg SLOW = 0);
     reg [23:0] COUNT= 24'b0 ; 
     always @ (posedge CLOCK) begin
         COUNT <= COUNT + 1;
-        SLOW <= (COUNT == 0) ? ~SLOW : SLOW;
+
 
